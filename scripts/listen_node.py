a5c7345f87486a62b43e0fd7159de2163cf323ec
Daniel McDonald <daniel.w.mcdonald@gmail.com>
2015-08-14 06:19:46 -0400

    modified to record audio in a background thread and limit number of samples to attempt speech to text conversion

--- scripts/listen_node.py
+++ scripts/listen_node.py
@@ -2,87 +2,127 @@
 from sys import byteorder
 from array import array
 from struct import pack
-import rospy
 from std_msgs.msg import String
+from subprocess import call
 
 import pyaudio
+import rospy
+import threading
+import time
+import traceback
 import wave
-from subprocess import call
 
 THRESHOLD = 10000
 CHUNK_SIZE = 1024
 FORMAT = pyaudio.paInt16
 RATE = 16000
-#RATE = 44100
-#RATE =8000
-is_listening = True
 
-maxval = -9999
-def is_silent(snd_data):
-    global maxval
-    "Returns 'True' if below the 'silent' threshold"
-    m = max(snd_data)
-    n = min(snd_data)
-    val = m - n
-    if (val > maxval):
-	maxval = val 
-    return val < THRESHOLD
+class Segment(object):
+    def __init__(self, sample_width, data):
+	self.sample_width = sample_width
+	self.data = data
 
-def record():
-    """
-    Record a word or words from the microphone and 
-    return the data as an array of signed shorts.
-    """
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=1, rate=RATE,
-        input=True,#output=True,
-        frames_per_buffer=CHUNK_SIZE, input_device_index=2)
+class ListenerThread(threading.Thread):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None):
+	threading.Thread.__init__(self, group=group, target=target, name=name, verbose=verbose)
+        self.args = args
+        self.kwargs = kwargs
+	self.p = None  # initialized in run()
+	self.stream = None  # initialized in run()
+	self.last_segment = None
+        return
 
-    num_silent = 0
-    snd_started = False
+    def run(self):
+	global shutdown
 
-    r = array('h')
+	input_device = 6
+	rospy.loginfo(rospy.get_caller_id() + ": starting audio listener for input device " + str(input_device))
 
-    rate = rospy.Rate(44100) # 44100 Hz - not a meaningful frequency, just a small delay
-    while 1:
-	rate.sleep() # give ROS a chance to run
-	if is_listening:
-		# little endian, signed short
-		snd_data = array('h', stream.read(CHUNK_SIZE))
+	self.p = pyaudio.PyAudio()
+	self.stream = self.p.open(format=FORMAT, channels=1, rate=RATE, input=True,
+				  frames_per_buffer=CHUNK_SIZE, input_device_index=input_device)
+	while not shutdown:
+	    # determine speech segment from input audio
+	    # store last speech segment recorded while listening
+	    self.last_segment = Segment(*self.record())
+	# close audio stream on exit
+	rospy.loginfo(rospy.get_caller_id() + ": stopping audio listener")
+	self.stream.stop_stream()
+	self.stream.close()
+	self.p.terminate()
+	rospy.loginfo(rospy.get_caller_id() + ": audio listener terminated")
+	return
+
+    def is_silent(self, snd_data):
+	"Returns 'True' if below the 'silent' threshold"
+	m = max(snd_data)
+	n = min(snd_data)
+	val = m - n
+	return val < THRESHOLD
+
+    def record(self):
+	"""
+	Record a segment of speech from the microphone and 
+	return the data as an array of signed shorts.
+	"""
+	global is_listening
+
+	num_silent = 0
+	snd_started = False
+
+	r = array('h')
+
+	rospy.loginfo(rospy.get_caller_id() + ": start recording")
+	while 1:
+	    # little endian, signed short
+	    snd_data = array('h', self.stream.read(CHUNK_SIZE))
+	    if is_listening:
 		if byteorder == 'big':
 		    snd_data.byteswap()
-		r.extend(snd_data)
 
-		silent = is_silent(snd_data)
+		silent = self.is_silent(snd_data)
+
+		if not silent and not snd_started:
+		    snd_started = True
+		    rospy.loginfo(rospy.get_caller_id() + ": sound started")
 
 		if silent and snd_started:
 		    num_silent += 1
-		elif not silent and not snd_started:
-		    snd_started = True
 
 		if snd_started and num_silent > 30:
+		    rospy.loginfo(rospy.get_caller_id() + ": sound ended")
 		    break
 
-    sample_width = p.get_sample_size(FORMAT)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+		if snd_started:
+		    r.extend(snd_data)
 
-    return sample_width, r
+	rospy.loginfo(rospy.get_caller_id() + ": stop recording, recorded " + str(len(r)))
+	sample_width = self.p.get_sample_size(FORMAT)
 
-def record_to_file(path):
-    "Records from the microphone and outputs the resulting data to 'path'"
-    sample_width, data = record()
-    data = pack('<' + ('h'*len(data)), *data)
+	return sample_width, r
+
+def record_to_file(segment, path):
+    "Write the speech segment data in WAV format to 'path'"
+    max_sound_samples = 200000  # TODO make this into a node parameter
+    size = len(segment.data)
+    if size > max_sound_samples:
+	rospy.loginfo(rospy.get_caller_id() + ": truncating sound length " + str(size) + " to " + str(max_sound_samples) + " samples")
+	segment.data = segment.data[:max_sound_samples + 1]
+	size = len(segment.data)
+	
+    rospy.loginfo(rospy.get_caller_id() + ": writing sound length " + str(size) + " width " + str(segment.sample_width) + " to " + path)
+    data = pack('<' + ('h'*size), *segment.data)
 
     wf = wave.open(path, 'wb')
     wf.setnchannels(1)
-    wf.setsampwidth(sample_width)
+    wf.setsampwidth(segment.sample_width)
     wf.setframerate(RATE)
     wf.writeframes(data)
     wf.close()
+    rospy.loginfo(rospy.get_caller_id() + ": wrote " + str(len(data)) + " bytes to " + path)
 
 def on_speech_info(msg):
+    global is_listening
     if msg.data == "start_speaking":
 	is_listening = False
         rospy.loginfo(rospy.get_caller_id() + ": stop listening")
@@ -93,19 +133,39 @@ def on_speech_info(msg):
         rospy.loginfo(rospy.get_caller_id() + ": unrecognised speech_info message %s", msg.data)
 
 if __name__ == '__main__':
-    global is_listening
+    global is_listening, shutdown
     is_listening = True
+    shutdown = False
+    rospy.init_node('listen_node')
     rospy.Subscriber("speech_info", String, on_speech_info)
     publisher = rospy.Publisher("recognized_speech", String)
-    rospy.init_node('listen_node')
 
-    while True:
-	print("please speak a word into the microphone")
-	record_to_file('demo.wav')
-	print("done - result written to demo.wav")
-	call(["./speech2text.sh"])
-	with open("stt.txt") as f:
-	    text = f.read()
-	    if text != "":
-		publisher.publish(text)
+    # initialize a thread to read and segment audio from the mic
+    # the thread should read continuously to avoid input buffer overflow on the audio stream
+    t = ListenerThread()
+    t.setDaemon(True)
+    t.start()
+
+    try:
+	print("in main thread")
+	rate = rospy.Rate(10)
+	while True:
+	    if t.last_segment is not None:
+		segment = t.last_segment
+		t.last_segment = None
+		record_to_file(segment, 'demo.wav')
+		call(["./speech2text.sh"])
+		with open("stt.txt") as f:
+		    text = str(f.read()).strip()
+		    if text != "":
+			publisher.publish(text)
+	    rate.sleep() # give ROS a chance to run
+    except KeyboardInterrupt:
+	pass
+    except:
+	traceback.print_exc()
+    print("set shutdown=True")
+    shutdown = True
+
+    t.join(1)
 
