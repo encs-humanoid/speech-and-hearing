d66519b9d70eb83921dac7171003b00daec1b5d0
Daniel McDonald <daniel.w.mcdonald@gmail.com>
2018-09-03 14:37:10 -0400

    rewrote speak_node.py to parameterize the voice characteristics

--- scripts/speak_node.py
+++ scripts/speak_node.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
-from subprocess import call
-from pipes import quote
-from time import sleep
-
 # Produce audible English speech given a text string.
 #
 # The Speak Node listens to the /say topic and passes the received
@@ -14,24 +8,52 @@ from time import sleep
 # speech recognition while the robot is speaking to avoid feedback
 # through the system.
 
-def on_say(msg):
-    publisher.publish("start_speaking")
-    sleep(0.5)
-    call(["espeak -v english-us -s 140 -p 80 -g 2 " + quote(msg.data) + " --stdout | aplay"], shell=True)
-    publisher.publish("done_speaking")
+import rospy
+from std_msgs.msg import String
+from subprocess import call
+from pipes import quote
+from time import sleep
 
-# Intializes everything
-def start():
-    global publisher
-    # starts the node
-    rospy.init_node('speak_node')
-    # subscribed to text inputs on topic "speech"
-    rospy.Subscriber("say", String, on_say)
-    publisher = rospy.Publisher("speech_info", String)
-    rospy.spin()
+
+class SpeakNode(object):
+    def __init__(self):
+	rospy.init_node('speak_node')
+
+	info_topic = self.get_param('~out_speech_info', '/speech_info')
+	say_topic = self.get_param('~in_say', '/say')
+	self.voice = self.get_param('~voice', 'english-us')
+	self.speed = int(self.get_param('~speed', '140'))
+	self.pitch = int(self.get_param('~pitch', '80'))
+
+	# the speech_info topic publishes messages when speaking starts and stops
+	self.publisher = rospy.Publisher(info_topic, String) #, queue_size=2)
+
+	# messages coming in on the say topic will be spoken aloud
+	rospy.Subscriber(say_topic, String, self.on_say)
+
+
+    def get_param(self, param_name, param_default):
+	value = rospy.get_param(param_name, param_default)
+	rospy.loginfo('Parameter %s has value %s', rospy.resolve_name(param_name), value)
+	return value
+
+
+    def run(self):
+    	rospy.spin()
+
+
+    def on_say(self, msg):
+	self.publisher.publish("start_speaking")
+	sleep(0.5)
+	espeak_cmd = "espeak -v %s -s %d -p %d -g 2 " % (self.voice, self.speed, self.pitch)
+	call([espeak_cmd + quote(msg.data) + " --stdout | aplay"], shell=True)
+	self.publisher.publish("done_speaking")
+
 
 if __name__ == '__main__':
-    start()
-
-
+    try:
+	node = SpeakNode()
+	node.run()
+    except rospy.ROSInterruptException:
+	pass
 
