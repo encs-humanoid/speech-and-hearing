bc956bdd59573046af7244ee511ec0e9455fcae0
Daniel McDonald <daniel.w.mcdonald@gmail.com>
2023-07-10 17:34:15 -0400

    Formatted for Python3 and added Whisper support

--- scripts/list_audio_devices.py
+++ scripts/list_audio_devices.py
@@ -2,10 +2,10 @@
 import pyaudio
 
 p = pyaudio.PyAudio()
-print "device count " + str(p.get_device_count())
+print("device count " + str(p.get_device_count()))
 
-for i in xrange(p.get_device_count()):
+for i in range(p.get_device_count()):
 	info = p.get_device_info_by_index(i)
-	print "Device " + str(i) + ": " + str(info)
+	print("Device " + str(i) + ": " + str(info))
 
 p.terminate()
